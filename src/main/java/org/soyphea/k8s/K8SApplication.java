7e041b08b166bd91b029281ee867d63207fdaf01
PedtritoAlejos <pedroalejosb@gmail.com>
2022-07-02 22:50:16 -0400

    vulnerability in code

--- src/main/java/org/soyphea/k8s/K8SApplication.java
+++ src/main/java/org/soyphea/k8s/K8SApplication.java
@@ -2,15 +2,7 @@ package org.soyphea.k8s;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
-import java.io.IOException;
 
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.soyphea.k8s.config.UserConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -29,30 +21,10 @@ public class K8SApplication implements CommandLineRunner {
         SpringApplication.run(K8SApplication.class, args);
         String xVaultToken = System.getenv("XVaultToken");
         String cityAppConfig = System.getenv("CityApp_Config");
-        System.out.println(xVaultToken);
-        System.out.println(cityAppConfig);
-        String user = "root";
-        String pass = "123456789";
-        byte[] salt = "notrandom".getBytes();
-
-        PBEParameterSpec cipherSpec = new PBEParameterSpec(salt, 10000); // Noncompliant, predictable salt
       
-		char[] chars = null;
-		PBEKeySpec spec = new PBEKeySpec(chars, salt, 10000, 256); // Noncompliant, predictable salt
+      
     }
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-       
-		String file = "users-card.csv";
-
-        File fileUnsafe = new File(file);
-        try {
-          FileUtils.forceDelete(fileUnsafe); // Noncompliant
-   
-        }
-        catch(IOException ex){
-          System.out.println (ex.toString());
-        }
-    }
+ 
   
 
     @Override
